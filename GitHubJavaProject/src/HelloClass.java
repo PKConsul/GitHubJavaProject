77ac7f62fb585f33272af85f8bd4bd88f91a4b02
PK Consulting <zlodziej.koni@gmail.com>
2012-01-25 17:09:29 +0100

    2nd commit

--- GitHubJavaProject/src/HelloClass.java
+++ GitHubJavaProject/src/HelloClass.java
@@ -1,4 +1,34 @@
 
 public class HelloClass {
 
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+
 }
